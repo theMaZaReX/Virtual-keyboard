01d34542161ecc8ceb3501798e11c347b58ce4c6
Dmitry <dmitri446@gmail.com>
2021-01-11 14:08:46 +0300

    added functionality to change the keyboard language

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const keyboard = {
-    elements:{
+    elements: {
         main: null,
         keysContainer: null,
         keys: []
     },
-    props:{
+    props: {
         textAreas: [],
         currentTextArea: null,
         value: "",
-        caps: false
+        caps: false,
+        changeLang: false
     },
     init() {
         _this = this;
@@ -19,53 +20,87 @@ const keyboard = {
         //setup main elements
         this.elements.main.classList.add('keyboard', 'keyboard--close');
         this.elements.keysContainer.classList.add('keyboard__keys');
-        
+
         //Add to DOM
         document.body.appendChild(this.elements.main);
         this.elements.main.appendChild(this.elements.keysContainer);
 
         //fill keyboard buttons
-        this.createKeys()
+        this.addKeys()
 
-        this.elements.keys = document.querySelectorAll('.keyboard__key');
+        this.elements.keys = document.querySelectorAll('keyboard__key');
 
         _this.props.textAreas = document.querySelectorAll('.keyboard-input');
 
-        _this.props.textAreas.forEach(function(element){
-            element.addEventListener('focus', function(event){
+        _this.props.textAreas.forEach(function (element) {
+            element.addEventListener('focus', function (event) {
                 _this.props.currentTextArea = event.target;
                 _this.elements.main.classList.remove('keyboard--close');
             })
         })
     },
+    createIcon(key) {
+        return icon = `<i class="material-icons">${key}</i>`;
+    },
+    toggleCaps() {
+        _this.props.caps = !_this.props.caps
+        this.elements.keys = document.querySelectorAll('.keyboard__key');
+        for (const key of _this.elements.keys) {
+            if (key.childElementCount === 0) {
+                key.textContent = _this.props.caps ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
+            }
+        }
+    },
+    fillTextArea() {
+        _this.props.currentTextArea.value = _this.props.value;
+    },
+    changeLang() {
+        _this.props.changeLang = !_this.props.changeLang;
+        _this.props.caps = false;
+        while (_this.elements.keysContainer.firstChild) {
+            _this.elements.keysContainer.removeChild(_this.elements.keysContainer.firstChild);
+        }
 
-    createKeys() {
-        _this = this;
-        
+        if (_this.props.changeLang){
         const keyLayout = [
             "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
-            "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
-            "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
-            "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+            "caps", "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х",
+             "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "enter",
+            "done", "lang", "я", "ч", "с", "м", "и", "т", "ь", "ъ", "б", "ю", ",", ".", "?",
             "space"
         ];
-        keyLayout.forEach(function(key){
+        let breakElements = ["backspace", "х", "enter", "?"];
+        _this.addKeys(keyLayout, breakElements);
+       
+    }
+    else{
+        _this.addKeys();  
+    }
+    },
+    addKeys(keyLayout = [
+        "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+        "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+        "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
+        "done", "lang", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+        "space"
+    ], breakElements = ['backspace', "p", "enter", "ю", "?"]) {
+        keyLayout.forEach(function (key) {
             const keyBtn = document.createElement('button');
-            const insertLineBreak = ['backspace', "p", "enter", "?"].indexOf(key) !== -1;
+            const insertLineBreak = breakElements.indexOf(key) !== -1;
             keyBtn.classList.add('keyboard__key');
             _this.elements.keysContainer.appendChild(keyBtn);
-            
-            if (insertLineBreak){
-                _this.elements.keysContainer.appendChild(document.createElement('br'));  
+
+            if (insertLineBreak) {
+                _this.elements.keysContainer.appendChild(document.createElement('br'));
             }
 
-            switch(key){
+            switch (key) {
                 case 'backspace':
                     keyBtn.insertAdjacentHTML('afterbegin', _this.createIcon('backspace'));
                     keyBtn.classList.add('keyboard__key--wide');
-                    keyBtn.addEventListener('click', function(){
-                       
-                        _this.props.value = _this.props.value.substring(0, _this.props.value.length-1);
+                    keyBtn.addEventListener('click', function () {
+
+                        _this.props.value = _this.props.value.substring(0, _this.props.value.length - 1);
                         _this.fillTextArea();
                     })
                     break;
@@ -73,62 +108,56 @@ const keyboard = {
                 case 'caps':
                     keyBtn.insertAdjacentHTML('afterbegin', _this.createIcon('keyboard_capslock'));
                     keyBtn.classList.add('keyboard__key--wide', 'keyboard__key-switch');
-                    keyBtn.addEventListener('click', function(){
+                    keyBtn.addEventListener('click', function () {
                         keyBtn.classList.toggle('keyboard__key-switch--active');
-                        _this.toggleCaps()});
+                        _this.toggleCaps()
+                    });
                     break;
 
                 case 'enter':
-                    keyBtn.insertAdjacentHTML('afterbegin', _this.createIcon('keyboard_return'));    
+                    keyBtn.insertAdjacentHTML('afterbegin', _this.createIcon('keyboard_return'));
                     keyBtn.classList.add('keyboard__key--wide');
-                    keyBtn.addEventListener('click', function(){
+                    keyBtn.addEventListener('click', function () {
                         _this.props.value += '\n';
                         _this.fillTextArea();
                     })
                     break;
 
                 case 'done':
-                    keyBtn.insertAdjacentHTML('afterbegin', _this.createIcon('check_circle'));    
+                    keyBtn.insertAdjacentHTML('afterbegin', _this.createIcon('check_circle'));
                     keyBtn.classList.add('keyboard__key--wide');
-                    keyBtn.addEventListener('click', function(){
+                    keyBtn.addEventListener('click', function () {
                         _this.elements.main.classList.add('keyboard--close');
                     })
                     break;
-                
+
                 case 'space':
-                    keyBtn.insertAdjacentHTML('afterbegin', _this.createIcon('space_bar'));    
+                    keyBtn.insertAdjacentHTML('afterbegin', _this.createIcon('space_bar'));
                     keyBtn.classList.add('keyboard__key--extra-wide');
-                    keyBtn.addEventListener('click', function(){
+                    keyBtn.addEventListener('click', function () {
                         _this.props.value += ' ';
                         _this.fillTextArea();
                     })
                     break;
 
+                case 'lang':
+                    keyBtn.insertAdjacentHTML('afterbegin', _this.createIcon('language'));
+                    keyBtn.classList.add('keyboard__key--small');
+                    keyBtn.addEventListener('click', function () {
+                        _this.changeLang();
+                    })
+                    break;
+
+
                 default:
                     keyBtn.textContent = key;
-                    keyBtn.addEventListener('click', function(){
+                    keyBtn.addEventListener('click', function () {
                         _this.props.value += keyBtn.textContent;
                         _this.fillTextArea();
                     })
             }
 
         })
-    },
-
-    createIcon(key){
-        return icon = `<i class="material-icons">${key}</i>`;
-    },
-    toggleCaps(){
-        _this.props.caps = !_this.props.caps
-
-        for (const key of _this.elements.keys){
-            if (key.childElementCount===0){
-            key.textContent = _this.props.caps ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
-        }
-        }
-    },
-    fillTextArea(){ 
-       _this.props.currentTextArea.value = _this.props.value;
     }
 
 }
